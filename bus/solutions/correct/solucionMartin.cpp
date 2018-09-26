a759b16db9f92752183cd3b8fe792a0d3447f8ff
Martin <martinmsanm@gmail.com>
2018-09-26 20:27:39 +0200

    solucion para bus

---
+++ bus/solutions/correct/solucionMartin.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int n,a=0,contador=0,b;
+cin>>n;
+
+while(a<n){
+	      
+	      cin>>b;
+	      if(b%2!=0 and b!=1){contador++;}
+	      a++;
+          }
+cout<<contador;
+
+return 0;            
+}
