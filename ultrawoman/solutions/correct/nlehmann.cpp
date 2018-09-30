c1e837144ab6ef4b10c86ca41eb942c62e0355d4
Nico Lehmann <nico.lehmannm@gmail.com>
2018-09-29 17:58:16 -0700

    Solucion megawoman

---
+++ ultrawoman/solutions/correct/nlehmann.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+long N, w, v, R;
+
+int main () {
+  cin >> N;
+  cin >> w >> v;
+  cin >> R;
+
+  for (int i = 0; i < R; ++i) {
+    long r, u;
+    cin >> r >> u;
+    // w + v*t = r + u*t
+    // t = (r - w)/(v - u)
+    int a = r - w;
+    int b = v - u;
+    if (b == 0 and v == u) {
+      cout << "NO\n";
+      return 0;
+    }
+    if (b !=0 && a % b == 0 && a/b > 0 && a/b < N) {
+      cout << "NO\n";
+      return 0;
+    }
+  }
+  cout << "SI\n";
+  return 0;
+}
