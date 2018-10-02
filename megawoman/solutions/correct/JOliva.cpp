e0186e0cc78483d484e868c65b0c1eda34d29f94
JavOliva <jav.oliva.silva@gmail.com>
2018-10-02 20:45:50 -0300

    Add files via upload

---
+++ megawoman/solutions/correct/JOliva.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);
+    long long N, w, v, R;
+    cin >> N >> w >> v >> R;
+
+    for (int i=0;i<R;i++){
+        long long wi, vi;
+        cin >> wi >> vi;
+        long long V=v-vi;
+        long long W=wi-w;
+        long long tfinal=(N-w+V-1)/V;
+        if (V==0){
+            if (W==0){
+                cout << "NO\n";return 0;
+            }
+        }
+        else if (W%V==0){
+            long long t=W/V;
+            if (t>=0 and t<tfinal){
+                cout << "NO\n";return 0;
+            }
+        }
+    }
+    cout << "YES\n";
+    return 0;
+}
